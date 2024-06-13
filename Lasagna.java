3f27d8157b21f922480f9f1f487e82305e92486d
Raven Hendricks <ravenkbeats@gmail.com>
2024-06-13 15:04:58 -0400

    added expectedMinutes functions and remainingMinutes functions

--- Lasagna.java
+++ Lasagna.java
@@ -1,7 +1,13 @@
 public class Lasagna {
     // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven(){
+        return 40;
+    }
 
     // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven(int actualMinutes){
+        return expectedMinutesInOven() - actualMinutes;
+    }
 
     // TODO: define the 'preparationTimeInMinutes()' method
 
