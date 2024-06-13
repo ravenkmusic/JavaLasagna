93f0d7d8cea5d11e98421abaffe95afd7d88182d
Raven Hendricks <ravenkbeats@gmail.com>
2024-06-13 15:19:01 -0400

    removed TODO

--- Lasagna.java
+++ Lasagna.java
@@ -1,21 +1,21 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
+    // define the 'expectedMinutesInOven()' method
     public int expectedMinutesInOven(){
         return 40;
     }
 
-    // TODO: define the 'remainingMinutesInOven()' method
+    // define the 'remainingMinutesInOven()' method
     public int remainingMinutesInOven(int actualMinutes){
         return expectedMinutesInOven() - actualMinutes;
     }
 
-    // TODO: define the 'preparationTimeInMinutes()' method
+    // define the 'preparationTimeInMinutes()' method
 
     public int preparationTimeInMinutes(int numOfLayers){
         return numOfLayers * 2;
     }
 
-    // TODO: define the 'totalTimeInMinutes()' method
+    // define the 'totalTimeInMinutes()' method
 
     public int totalTimeInMinutes(int prepTime, int actualMinutes){
         return preparationTimeInMinutes(prepTime) + actualMinutes;
